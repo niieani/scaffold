f68e0a1c7de2180b7fff7a5d10e360e30e2909a5
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2024-11-10 16:25:59 -0800

    fix: webpack's ts-loader uses running directory for context

--- packages/scaffold/src/configs/webpack.ts
+++ packages/scaffold/src/configs/webpack.ts
@@ -79,6 +79,7 @@ export = ({
           use: [
             {
               loader: 'ts-loader',
+              context: process.cwd(),
               options: {
                 transpileOnly: true,
                 compilerOptions: {
